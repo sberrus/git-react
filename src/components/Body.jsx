a211c4f8c7db539d47459b5735fdb0e71fcf7ba6
samdev.es <samuelberrus@gmail.com>
2021-03-08 20:17:24 +0100

    Curso de Git desde 0

---
+++ src/components/Body.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+function Body() {
+  return (
+    <div className="container p-0">
+      <div className="container-fluid" id="body">
+        <div className="container-fluid p-0">
+          <h3>Profesor Bluuweb</h3>
+          <a
+            href="https://www.youtube.com/channel/UCH7IANkyEcsVW_y1IlpkamQ"
+            target="_BLANK"
+          >
+            <h6>Bluuweb - YouTube</h6>
+          </a>
+        </div>
+        <div className="container-fluid">
+            <h2>Snippets y definiciones que tengo que reforzar</h2>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Body;
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <div className="container border-bottom border-dark">
+          <h1 className="fw-normal">Clase de Git y GitHub básico de Bluuweb con</h1>
+          <h2 className="fw-light text-end">Aprendiendo Git básico</h2>
+        </div>
+    )
+}
+
+export default Header
